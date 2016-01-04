4b072a52f9d268105eb1cb7c121a9ca71aec1f2c
unknown <cpersaud@DEV-CPERSAUD-US.cortera.local>
2016-01-04 12:34:31 -0500

    made dummy code more dummy-like

--- hvacDummy.py
+++ hvacDummy.py
@@ -6,34 +6,32 @@ import time
 import json
 import os
 import dotenv
-import RPi.GPIO as GPIO
-import Adafruit_DHT
+# import RPi.GPIO as GPIO
+# import Adafruit_DHT
 
-def dht22Reading():
-    # Sensor should be set to Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
-    sensor = Adafruit_DHT.DHT22
-    pin = os.environ.get('DHT22_PIN')
+# def dht22Reading():
+#     # Sensor should be set to Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+#     sensor = Adafruit_DHT.DHT22
+#     pin = os.environ.get('DHT22_PIN')
 
-    # Try to grab a sensor reading.  
-    # Use the read_retry method which will retry up to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    rawReading = {
-        'humidity': humidity,
-        'temperature': temperature  # Raw temperature reading is in Celsius
-    }
+#     # Try to grab a sensor reading.  
+#     # Use the read_retry method which will retry up to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+#     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+#     rawReading = {
+#         'humidity': humidity,
+#         'temperature': temperature  # Raw temperature reading is in Celsius
+#     }
 
-    return rawReading
+#     return rawReading
 
 def currentTemperatureRaw():
-    rawReading = dht22Reading()
-    return rawReading['temperature']
+    return random.uniform(60.0, 90.0)
 
-def convertToC(tempF):
-    return ((tempF - 32.0) / 9.0) * 5.0  # Convert Fahrenheit to Celsius. Remember to add '.0' after numbers to make them floats. We want this to be a float.
+# def convertToC(tempF):
+#     return ((tempF - 32.0) / 9.0) * 5.0  # Convert Fahrenheit to Celsius. Remember to add '.0' after numbers to make them floats. We want this to be a float.
 
 def currentHumidityRaw():
-    rawReading = dht22Reading()
-    return rawReading['humidity']
+    return random.uniform(60.0, 90.0)
 
 def sendCurrentStatus():
     dataToSend = {
@@ -82,8 +80,6 @@ def setStatus(coolSwitch, coolTemperature, heatSwitch, heatTemperature, fanSwitc
     # Maybe get min/max settings from user/frontend?
     minTemp = float( os.environ.get('MINIMUM_TEMPERATURE') )
     maxTemp = float( os.environ.get('MAXIMUM_TEMPERATURE') )
-    minTemp = convertToC(minTemp)
-    maxTemp = convertToC(maxTemp)
 
     # Example of how `tempBuffer` is used: If COOL is set to 23.889C (about 75F), it will not turn on if tempF is 23.89, but only when it reaches 24.444 (about 76F)
     tempBuffer = float( os.environ.get('TEMPERATURE_BUFFER') ) # One degree change in Fahrenheit is about 0.555 in Celsius. So if I want the temperature buffer to be one degree Fahrenheit, set this to 0.555 Celsius.
@@ -98,7 +94,7 @@ def setStatus(coolSwitch, coolTemperature, heatSwitch, heatTemperature, fanSwitc
 
     if coolSwitch==1:
         if inTemperatureRange(minTemp, maxTemp, coolTemperature)==False:
-            print 'Cool temperature out of range'
+            # print 'Cool temperature out of range'
             coolSwitch = 0
             coolTemperature = None
         else:
@@ -108,17 +104,17 @@ def setStatus(coolSwitch, coolTemperature, heatSwitch, heatTemperature, fanSwitc
 
             if roomTemperature > float(coolTemperature)+tempBuffer:
                     coolSwitch = 1
-                    print 'Room temperature too high. Cooling...'
+                    # print 'Room temperature too high. Cooling...'
             else:
                     coolSwitch = 0
-                    print 'Room temp cool, turning off cool.'
+                    # print 'Room temp cool, turning off cool.'
     else:
             coolSwitch = 0
-            print 'cool switched off'
+            # print 'cool switched off'
 
     if heatSwitch==1:
         if inTemperatureRange(minTemp, maxTemp, heatTemperature)==False:
-            print 'Heat temp out of range'
+            # print 'Heat temp out of range'
             heatSwitch = 0
             heatTemperature = None
         else:
@@ -128,39 +124,39 @@ def setStatus(coolSwitch, coolTemperature, heatSwitch, heatTemperature, fanSwitc
 
             if roomTemperature < float(heatTemperature)-tempBuffer:
                     heatSwitch = 1
-                    print 'Room temp too cold. Heating...'
+                    # print 'Room temp too cold. Heating...'
             else:
                     heatSwitch = 0
-                    print 'Room temp warm. Turning off heat.'
+                    # print 'Room temp warm. Turning off heat.'
     else:
         heatSwitch = 0
-        print 'heat switched off'
+        # print 'heat switched off'
 
 
-    if fanSwitch==0:
-        GPIO.output(fanPin, GPIO.HIGH)
-        print 'fan switched off'
-    elif fanSwitch==1:
-        GPIO.output(fanPin, GPIO.LOW)
-        print 'fan switched on'
+    # if fanSwitch==0:
+    #     GPIO.output(fanPin, GPIO.HIGH)
+    #     print 'fan switched off'
+    # elif fanSwitch==1:
+    #     GPIO.output(fanPin, GPIO.LOW)
+    #     print 'fan switched on'
 
-    if coolSwitch==0:
-        GPIO.output(coolPin, GPIO.HIGH)
-    elif coolSwitch==1:
-        GPIO.output(coolPin, GPIO.LOW)
+    # if coolSwitch==0:
+    #     GPIO.output(coolPin, GPIO.HIGH)
+    # elif coolSwitch==1:
+    #     GPIO.output(coolPin, GPIO.LOW)
             
-    if heatSwitch==0:
-        GPIO.output(heatPin, GPIO.HIGH)
-    elif heatSwitch==1:
-        GPIO.output(heatPin, GPIO.LOW)                
+    # if heatSwitch==0:
+    #     GPIO.output(heatPin, GPIO.HIGH)
+    # elif heatSwitch==1:
+    #     GPIO.output(heatPin, GPIO.LOW)                
 
     currentStatus['coolSwitch'] = coolSwitch
     currentStatus['coolTemperature'] = coolTemperature
     currentStatus['heatSwitch'] = heatSwitch
     currentStatus['heatTemperature'] = heatTemperature
     currentStatus['fanSwitch'] = fanSwitch
-    print currentStatus # for debugging
-    print '================'
+    # print currentStatus # for debugging
+    # print '================'
 
 def inTemperatureRange(minTemp, maxTemp, temperature):
     if temperature==None or temperature=='':
@@ -182,21 +178,21 @@ if __name__=='__main__':
 
 
 
-    # Pin Definitons:
-    coolPin = int( os.environ.get('COOL_PIN') )
-    heatPin = int( os.environ.get('HEAT_PIN') )
-    fanPin = int( os.environ.get('FAN_PIN') )
-    hvacPinArray = [
-        coolPin,
-        heatPin,
-        fanPin
-    ]
+    # # Pin Definitons:
+    # coolPin = int( os.environ.get('COOL_PIN') )
+    # heatPin = int( os.environ.get('HEAT_PIN') )
+    # fanPin = int( os.environ.get('FAN_PIN') )
+    # hvacPinArray = [
+    #     coolPin,
+    #     heatPin,
+    #     fanPin
+    # ]
 
-    # Pin Setup:
-    GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-    for pin in hvacPinArray:
-        GPIO.setup(pin, GPIO.OUT) # When Pi interacts with pin, it is to send data from Pi to whatever is attached to pin
-        GPIO.output(pin,GPIO.HIGH) # Set pin to 'HIGH', which means 'off'
+    # # Pin Setup:
+    # GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+    # for pin in hvacPinArray:
+    #     GPIO.setup(pin, GPIO.OUT) # When Pi interacts with pin, it is to send data from Pi to whatever is attached to pin
+    #     GPIO.output(pin,GPIO.HIGH) # Set pin to 'HIGH', which means 'off'
 
     # Get current status
     baseURL = os.environ.get('BASE_URL')
@@ -232,5 +228,5 @@ if __name__=='__main__':
             lastConnect = datetime.datetime.now()
             time.sleep(1)
     except Exception as e:
-        GPIO.cleanup() # cleanup all GPIO, which will turn off everything attached to GPIO pins
+        # GPIO.cleanup() # cleanup all GPIO, which will turn off everything attached to GPIO pins
         raise
